7002f503f72b8d3e2f9d20dee7fab47abbee02b1
Rohit Durvasula <rohit.durvasula@coinbase.com>
2023-12-18 12:48:23 -0800

    Release v0.2.0

---
+++ src/gen/coinbase/staking/v1alpha1/protocol.pb.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+export type Protocol = {
+  name?: string
+}
+
+export type ListProtocolsRequest = {
+}
+
+export type ListProtocolsResponse = {
+  protocols?: Protocol[]
+}

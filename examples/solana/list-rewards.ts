5225b08df657318f31c6cb88bb1433e2626265da
Shane O'Brien <sobrien@bisontrails.co>
2024-05-10 15:12:57 -0400

    Update rewards examples with new client version

--- examples/solana/list-rewards.ts
+++ examples/solana/list-rewards.ts
@@ -1,9 +1,13 @@
 import { StakingClient } from '../../src/client/staking-client';
 
-// TODO: Replace address as per your requirement.
+// Address can be substituted with any Solana validator.
 const address: string = 'beefKGBWeSpHzYBHZXwp5So7wdQGX6mu4ZHCsH3uTar';
 
-const client = new StakingClient();
+// Set your api key name and private key here. Get your keys from here: https://portal.cdp.coinbase.com/access/api
+const apiKeyName: string = 'your-api-key-name';
+const apiPrivateKey: string = 'your-api-private-key';
+
+const client = new StakingClient(apiKeyName, apiPrivateKey);
 
 async function listRewards(): Promise<void> {
   if (address === '') {

10d60540575acd4bcb03b93cd6aa4e9136e395a5
Shane O'Brien <32044819+ProfMoo@users.noreply.github.com>
2024-05-10 15:19:51 -0400

    Update Rewards Examples API Key Usage (#35)
    This examples needed updating to work with the `StakingClient` instantiation in the 0.8.x releases.

--- examples/cosmos/list-rewards.ts
+++ examples/cosmos/list-rewards.ts
@@ -1,10 +1,15 @@
 import { StakingClient } from '../../src/client/staking-client';
 
-// Defines which address and rewards we want to see
+// Address can be substituted with any Cosmos validator.
 const address: string = 'cosmosvaloper1c4k24jzduc365kywrsvf5ujz4ya6mwympnc4en';
+
 const filter: string = `address='${address}' AND period_end_time > '2024-03-25T00:00:00Z' AND period_end_time < '2024-03-27T00:00:00Z'`;
 
-const client = new StakingClient();
+// Set your api key name and private key here. Get your keys from here: https://portal.cdp.coinbase.com/access/api
+const apiKeyName: string = 'your-api-key-name';
+const apiPrivateKey: string = 'your-api-private-key';
+
+const client = new StakingClient(apiKeyName, apiPrivateKey);
 
 // Loops through rewards array and prints each reward
 client.Cosmos.listRewards(filter).then((resp) => {
--- examples/ethereum/list-rewards.ts
+++ examples/ethereum/list-rewards.ts
@@ -1,11 +1,16 @@
 import { StakingClient } from '../../src/client/staking-client';
 
-// Defines which address and rewards we want to see
+// Address can be substituted with any Ethereum validator.
 const address: string =
   '0xac53512c39d0081ca4437c285305eb423f474e6153693c12fbba4a3df78bcaa3422b31d800c5bea71c1b017168a60474';
+
 const filter: string = `address='${address}' AND period_end_time > '2024-02-25T00:00:00Z' AND period_end_time < '2024-02-27T00:00:00Z'`;
 
-const client = new StakingClient();
+// Set your api key name and private key here. Get your keys from here: https://portal.cdp.coinbase.com/access/api
+const apiKeyName: string = 'your-api-key-name';
+const apiPrivateKey: string = 'your-api-private-key';
+
+const client = new StakingClient(apiKeyName, apiPrivateKey);
 
 // Loops through rewards array and prints each reward
 client.Ethereum.listRewards(filter).then((resp) => {

71dfd678a8b5939e842dac666987432080a21823
Marcin Lenczewski <marcin.lenczewski@coinbase.com>
2024-05-16 13:38:20 -0400

    Lint fixes and package update for linter

--- examples/ethereum/list-rewards.ts
+++ examples/ethereum/list-rewards.ts
@@ -7,16 +7,14 @@ const apiPrivateKey: string = 'your-api-private-key';
 const client = new StakingClient(apiKeyName, apiPrivateKey);
 
 // Defines which address and rewards we want to see
-const address: string =
-  '0x60c7e246344ae3856cf9abe3a2e258d495fc39e0';
+const address: string = '0x60c7e246344ae3856cf9abe3a2e258d495fc39e0';
 const filter: string = `address='${address}' AND period_end_time > '2024-05-01T00:00:00Z' AND period_end_time < '2024-05-02T00:00:00Z'`;
 
 // Loops through rewards array and prints each reward
-var list = async function () {
-  const resp = await client.Ethereum.listRewards(filter)
+(async (): Promise<void> => {
+  const resp = await client.Ethereum.listRewards(filter);
+  // eslint-disable-next-line @typescript-eslint/padding-line-between-statements
   resp.rewards!.forEach((reward) => {
     console.log(JSON.stringify(reward, null, 2));
   });
-}
-
-list();
+})();
